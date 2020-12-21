ca18738c3ccd2031f86cd3b66aeb9fb5d43e8dcd
Sebastian Haas <sehaas@deebas.com>
2020-12-21 20:54:42 +0100

    fix movescount login
    - use https
    - use new api endpoints
    - fix cookie handling

--- suuxs.sh
+++ suuxs.sh
@@ -3,27 +3,28 @@
 USERAGENT="suuxs.sh"
 COOKIES_TXT="suuxs.cookies"
 CLEAR_COOKIES="rm -f '${COOKIES_TXT}'"
-SUUXS="curl --silent --cookie-jar ${COOKIES_TXT} --cookie ${COOKIES_TXT} -A 'suuxs.sh'"
-URL_OVERVIEW="http://www.movescount.com/overview"
+SUUXS="curl --silent --cookie-jar ${COOKIES_TXT} --cookie ${COOKIES_TXT} -A '${USERAGENT}'"
+URL_OVERVIEW="https://www.movescount.com/overview"
+URL_LOGIN_PAGE="https://www.movescount.com/auth?redirect_uri=%2foverview"
 URL_PREAUTH="https://servicegate.suunto.com/UserAuthorityService/Authenticate?service=Movescount"
 URL_AUTH="https://www.movescount.com/services/UserAuthenticated"
 
 if [ ! -f "./suuxs.rc" ]; then
-	echo "ERROR: missing configuration file 'suuxsrc'"
+	echo "ERROR: missing configuration file 'suuxs.rc'"
 	exit 1
 else
-	. ./suuxsrc
+	. ./suuxs.rc
 fi
-URL_GETMOVES="http://activityrecordsapi-prod.eu-west-1.elasticbeanstalk.com/activityrecordsapi/moves/getmoves?startDateString=${STARTDATE}&endDateString=${ENDDATE}&userId="
-URL_EXPORTMOVE="http://www.movescount.com/move/export?format=${FORMAT}&id="
-
+URL_GETMOVES="https://uiservices.movescount.com/activityrecordsapi/moves/getmoves?startDateString=${STARTDATE}&endDateString=${ENDDATE}&userId="
+URL_EXPORTMOVE="https://www.movescount.com/move/export?format=${FORMAT}&id="
 
 LOGGED_IN=$(${SUUXS} -o /dev/null -w "%{http_code}" "${URL_OVERVIEW}")
 if [ "200" -ne "${LOGGED_IN}" ]; then
 	echo "retry login"
 	${CLEAR_COOKIES}
-	TOKEN=$(${SUUXS} -d "{\"EmailAddress\":\"${USERNAME}\",\"Password\":\"${PASSWORD}\"}" -H "Content-Type: application/json" "${URL_PREAUTH}")
+	${SUUXS} -o /dev/null "${URL_LOGIN_PAGE}"
 
+	TOKEN=$(${SUUXS} -d "{\"EmailAddress\":\"${USERNAME}\",\"Password\":\"${PASSWORD}\"}" -H "Content-Type: application/json" "${URL_PREAUTH}")
 	LOGGED_IN=$(${SUUXS} -d "{\"token\":${TOKEN},\"utcOffset\":\"60\",\"redirectUri\":\"/overview\"}" -H "Content-Type: application/json" "${URL_AUTH}" | jq -r '.d.Value' )
 	if [ "/overview" != "${LOGGED_IN}" ]; then
 		${CLEAR_COOKIES}
